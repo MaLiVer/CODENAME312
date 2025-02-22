798a8e531454ee954becba8bb3bad63d539f2ee8
Виктор Маливер <maliverko@gmail.com>
2025-02-23 03:35:49 +0500

    добавил основу для сервера

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Server
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // запускам сервак в отдельном потоке, чтобы консоль была доступна
+            Server server = new Server();
+
+            string? command;
+            // цикл для обработки консольных команд
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command?.ToLower() == "stop")
+                {
+                    // остановка сервера
+                    server.isRunning = false;
+                    Console.WriteLine("Сервер остановлен.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда.");
+                }
+            }
         }
     }
 }
---
+++ Server/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Server
+{
+    class Server
+    {
+        public static bool isRunning = true;
+
+        // конструктор сервера
+        public Server()
+        {
+            // запускам сервак в отдельном потоке, чтобы консоль была доступна
+            var serverTask = Task.Run(() => RunServerAsync());
+        }
+
+        // поток сервака
+        static async Task RunServerAsync()
+        {
+            // старт сервака
+            TcpListener server = new TcpListener(IPAddress.Any, 45014);
+            server.Start();
+
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            // ждем подключений клиентов
+            while (isRunning)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                var taskClient = RunClientSessionAsync(client);
+            }
+        }
+
+        static async Task RunClientSessionAsync(TcpClient client)
+        {
+
+        }
+    }
+}
